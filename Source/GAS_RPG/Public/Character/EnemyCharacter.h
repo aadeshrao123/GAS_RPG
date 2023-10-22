c41674ec2b1ffecbf9ed55442aa27e234e3caee5
Aadesh Yadav <55663044+aadeshrao123@users.noreply.github.com>
2023-10-22 19:26:47 +0530

    Added a new Interface class for Highlighting the enemy while Hovering on it. Just created the class and it pure virtual function.

--- Source/GAS_RPG/Public/Character/EnemyCharacter.h
+++ Source/GAS_RPG/Public/Character/EnemyCharacter.h
@@ -4,14 +4,19 @@
 
 #include "CoreMinimal.h"
 #include "Character/RPG_BaseCharacter.h"
+#include "Interaction/EnemyInterface.h"
 #include "EnemyCharacter.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class GAS_RPG_API AEnemyCharacter : public ARPG_BaseCharacter
+class GAS_RPG_API AEnemyCharacter : public ARPG_BaseCharacter, public IEnemyInterface
 {
 	GENERATED_BODY()
+
+public:
+	virtual void HighlightActor() override;
+	virtual void UnHighlightActor() override;
 	
 };

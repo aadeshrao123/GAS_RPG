5d2b166f988d4b378f86f37b43596de445231bc2
Aadesh Yadav <55663044+aadeshrao123@users.noreply.github.com>
2023-10-25 22:37:20 +0530

    Created new C++ for player state in unreal engine just that nothing special.

---
+++ Source/GAS_RPG/Private/Player/Hero_PlayerState.cpp
@@ -0,0 +1,9 @@
+﻿// Copyright AadeshRao
+
+
+#include "Player/Hero_PlayerState.h"
+
+AHero_PlayerState::AHero_PlayerState()
+{
+	NetUpdateFrequency = 100.f; //For setting the value of how often server should update
+}
---
+++ Source/GAS_RPG/Public/Player/Hero_PlayerState.h
@@ -0,0 +1,19 @@
+﻿// Copyright AadeshRao
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "Hero_PlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_RPG_API AHero_PlayerState : public APlayerState
+{
+	GENERATED_BODY()
+
+public:
+	AHero_PlayerState();
+};

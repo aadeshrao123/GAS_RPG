71d1c8d300ba92b931023c8af106f88923526c3d
Aadesh Yadav <55663044+aadeshrao123@users.noreply.github.com>
2023-10-26 23:36:43 +0530

    I've added an AffectActor to modify our Attribute sets. Currently, I'm using an illegal approach, but it's only for testing purposes. Eventually, I will replace this with Gameplay Effects, which is the correct method.

---
+++ Source/GAS_RPG/Private/Actor/AffectActor.cpp
@@ -0,0 +1,56 @@
+﻿// Copyright AadeshRao
+
+
+#include "Actor/AffectActor.h"
+
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemInterface.h"
+#include "NavigationSystemTypes.h"
+#include "AbilitySystem/Base_AttributeSet.h"
+#include "Components/SphereComponent.h"
+
+
+AAffectActor::AAffectActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	SetRootComponent(Mesh);
+
+	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
+	Sphere->SetupAttachment(Mesh);
+}
+
+void AAffectActor::OnOverlap(UPrimitiveComponent* OverlappedComponent,
+	AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+	const FHitResult& SweepResult)
+{
+	// Change this to apply a Gameplay Effect. Using const_Cast is just for test Attribute Sets
+	if(IAbilitySystemInterface* ASCInterface = Cast<IAbilitySystemInterface>(OtherActor))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("CastSucceded"));
+
+		const UBase_AttributeSet* HeroAttributeSet = Cast<UBase_AttributeSet>(ASCInterface->GetAbilitySystemComponent()->GetAttributeSet(UBase_AttributeSet::StaticClass()));
+		
+		UBase_AttributeSet* MutableHeroAttributeSet = const_cast<UBase_AttributeSet*>(HeroAttributeSet);
+		MutableHeroAttributeSet->SetHealth(HeroAttributeSet->GetHealth() + 25.f);
+		Destroy();
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("CastFailed"));
+	}
+}
+
+void AAffectActor::EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+}
+
+void AAffectActor::BeginPlay()
+{
+	Super::BeginPlay();
+	Sphere->OnComponentBeginOverlap.AddDynamic(this, &AAffectActor::OnOverlap);
+	Sphere->OnComponentEndOverlap.AddDynamic(this, &AAffectActor::EndOverlap);
+	
+}
---
+++ Source/GAS_RPG/Public/Actor/AffectActor.h
@@ -0,0 +1,34 @@
+﻿// Copyright AadeshRao
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "AffectActor.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class GAS_RPG_API AAffectActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AAffectActor();
+	UFUNCTION()
+	virtual void OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+
+	UFUNCTION()
+	virtual void EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USphereComponent> Sphere;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent> Mesh;
+
+};

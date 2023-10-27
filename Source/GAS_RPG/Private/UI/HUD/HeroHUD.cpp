f7936c51dc7e36c81d27668521bce114c34d8723
Aadesh Yadav <55663044+aadeshrao123@users.noreply.github.com>
2023-10-27 12:13:21 +0530

    Added a AHUD class in c++ and called create Widget for our WBP_Overlay to show our widget on the viewport

---
+++ Source/GAS_RPG/Private/UI/HUD/HeroHUD.cpp
@@ -0,0 +1,12 @@
+﻿// Copyright AadeshRao
+
+
+#include "UI/HUD/HeroHUD.h"
+#include "UI/Widget/HeroUserWidget.h"
+
+void AHeroHUD::BeginPlay()
+{
+	Super::BeginPlay();
+	UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld(), OverlayWidgetClass);
+	Widget->AddToViewport();
+}
---
+++ Source/GAS_RPG/Public/UI/HUD/HeroHUD.h
@@ -0,0 +1,28 @@
+﻿// Copyright AadeshRao
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "HeroHUD.generated.h"
+
+class UHeroUserWidget;
+/**
+ * 
+ */
+UCLASS()
+class GAS_RPG_API AHeroHUD : public AHUD
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY()
+	TObjectPtr<UHeroUserWidget> OverlayWidget;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UHeroUserWidget> OverlayWidgetClass;
+};

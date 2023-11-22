44909688ff36b577f66e01ab30fb2bf0567db140
Aadesh Yadav <55663044+aadeshrao123@users.noreply.github.com>
2023-11-22 12:38:06 +0530

    Added GameplayModMagnitudeCalculation class for calculating the Secondary Attributes MaxHealth and MaxMana

--- Source/GAS_RPG/Private/Character/RPG_BaseCharacter.cpp
+++ Source/GAS_RPG/Private/Character/RPG_BaseCharacter.cpp
@@ -32,7 +32,8 @@ void ARPG_BaseCharacter::ApplyEffectToSelf(TSubclassOf<UGameplayEffect> Gameplay
 {
 	check(IsValid(AbilitySystemComponent));
 	check(GameplayEffectClass);
-	const FGameplayEffectContextHandle ContextHandle = GetAbilitySystemComponent()->MakeEffectContext();
+	FGameplayEffectContextHandle ContextHandle = GetAbilitySystemComponent()->MakeEffectContext();
+	ContextHandle.AddSourceObject(this);
 	const FGameplayEffectSpecHandle SpecHandle = GetAbilitySystemComponent()->MakeOutgoingSpec(GameplayEffectClass, Level, ContextHandle);
 	GetAbilitySystemComponent()->ApplyGameplayEffectSpecToTarget(*SpecHandle.Data.Get(), GetAbilitySystemComponent());
 }

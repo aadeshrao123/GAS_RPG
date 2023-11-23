8edd816b0c57b501d7f3bdd752147a72a57e8119
Aadesh Yadav <55663044+aadeshrao123@users.noreply.github.com>
2023-11-23 12:41:11 +0530

    Implemented Gameplay Tag Lookup Functionality in DataAsset Cpp File
    Created the UAttributeInfo DataAsset class with a struct FRPGAttributeInfo to store AttributeTag, AttributeName, AttributeDescription, and AttributeValue. Implemented the FindAttributeInfoForTag function in the associated CPP file to search for attribute information based on a specified GameplayTag. The function returns the corresponding struct if the tag is found, and if not, it logs an error message and return a empty struct.

---
+++ Source/GAS_RPG/Private/AbilitySystem/Data/AttributeInfo.cpp
@@ -0,0 +1,23 @@
+﻿// Copyright AadeshRao
+
+
+#include "AbilitySystem/Data/AttributeInfo.h"
+
+FRPGAttributeInfo UAttributeInfo::FindAttributeInfoForTag(const FGameplayTag& AttributeTag, bool bLogNotFound) const
+{
+	for (const FRPGAttributeInfo& Info : AttributeInformation)
+	{
+		if (Info.AttributeTag.MatchesTagExact(AttributeTag))
+		{
+			bLogNotFound = true;
+			return Info;
+		}
+		bLogNotFound = false;
+	}
+	if (!bLogNotFound)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Can't find info for AttributeTag [%s] on AttributeInfo [%s]."), *AttributeTag.ToString(), * GetNameSafe(this));
+	}
+
+	return FRPGAttributeInfo();
+}
---
+++ Source/GAS_RPG/Public/AbilitySystem/Data/AttributeInfo.h
@@ -0,0 +1,44 @@
+﻿// Copyright AadeshRao
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Engine/DataAsset.h"
+#include "AttributeInfo.generated.h"
+
+
+USTRUCT(BlueprintType)
+struct FRPGAttributeInfo
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FGameplayTag AttributeTag;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FText AttributeName = FText();
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FText AttributeDescription = FText();
+
+	UPROPERTY(BlueprintReadOnly)
+	float AttributeValue = 0.f;
+};
+
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_RPG_API UAttributeInfo : public UDataAsset
+{
+	GENERATED_BODY()
+public:
+
+	FRPGAttributeInfo FindAttributeInfoForTag(const FGameplayTag& AttributeTag, bool bLogNotFound = false) const;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TArray<FRPGAttributeInfo> AttributeInformation;
+	
+};

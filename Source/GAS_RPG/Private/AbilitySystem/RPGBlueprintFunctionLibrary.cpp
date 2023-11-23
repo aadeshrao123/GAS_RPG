ca3a25de1e80887670f055e1c38f79f149c0fee5
Aadesh Yadav <55663044+aadeshrao123@users.noreply.github.com>
2023-11-23 21:31:58 +0530

    Added a Blueprint function Library by and added a function for getting OverlayWidgetController in it.

---
+++ Source/GAS_RPG/Private/AbilitySystem/RPGBlueprintFunctionLibrary.cpp
@@ -0,0 +1,26 @@
+﻿// Copyright AadeshRao
+
+
+#include "AbilitySystem/RPGBlueprintFunctionLibrary.h"
+
+#include "Kismet/GameplayStatics.h"
+#include "Player/Hero_PlayerState.h"
+#include "UI/HUD/HeroHUD.h"
+#include "UI/WidgetController/HeroWidgetController.h"
+
+UOverlayWidgetController* URPGBlueprintFunctionLibrary::GetOverlayWidgetController(const UObject* WorldContextObject)
+{
+	if (APlayerController* PC = UGameplayStatics::GetPlayerController(WorldContextObject, 0))
+	{
+		if (AHeroHUD* HeroHUD = Cast<AHeroHUD>(PC->GetHUD()))
+		{
+			AHero_PlayerState* PS = PC->GetPlayerState<AHero_PlayerState>();
+			UAbilitySystemComponent* ASC = PS->GetAbilitySystemComponent();
+			UAttributeSet* AS = PS->GetAttributeSet();
+
+			const FWidgetControllerParams WCParams(PC, PS, ASC, AS);
+			return HeroHUD->GetOverlayWidgetController(WCParams);
+		}
+	}
+	return nullptr;
+}
---
+++ Source/GAS_RPG/Public/AbilitySystem/RPGBlueprintFunctionLibrary.h
@@ -0,0 +1,21 @@
+﻿// Copyright AadeshRao
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "RPGBlueprintFunctionLibrary.generated.h"
+
+class UOverlayWidgetController;
+/**
+ * 
+ */
+UCLASS()
+class GAS_RPG_API URPGBlueprintFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+
+	UFUNCTION(BlueprintPure, Category = "RPGAbilitySystemLibrary | WidgetController")
+	static UOverlayWidgetController* GetOverlayWidgetController(const UObject* WorldContextObject);
+};

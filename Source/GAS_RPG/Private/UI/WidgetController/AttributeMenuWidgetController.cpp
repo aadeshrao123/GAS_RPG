46cdfc2a847faa9c4c5fe5526d7b82582431fd14
Aadesh Yadav <55663044+aadeshrao123@users.noreply.github.com>
2023-11-23 12:50:07 +0530

    Created a New Widget Controller for our Attribute Menu named AttributeMenuWidgetController derrived from the class HeroWidgetController and ovridden some virtual function named BrodcastInitialValues and BindCallbacksToDependencies

---
+++ Source/GAS_RPG/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+﻿// Copyright AadeshRao
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksTODependencies()
+{
+	Super::BindCallbacksTODependencies();
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+	Super::BroadcastInitialValues();
+}
---
+++ Source/GAS_RPG/Public/UI/WidgetController/AttributeMenuWidgetController.h
@@ -0,0 +1,19 @@
+﻿// Copyright AadeshRao
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "HeroWidgetController.h"
+#include "AttributeMenuWidgetController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_RPG_API UAttributeMenuWidgetController : public UHeroWidgetController
+{
+	GENERATED_BODY()
+public:
+	virtual void BindCallbacksTODependencies() override;
+	virtual void BroadcastInitialValues() override;
+};

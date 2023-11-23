1a0dbf9d75eda07f032244c783f22ac10483da9d
Aadesh Yadav <55663044+aadeshrao123@users.noreply.github.com>
2023-11-23 12:17:38 +0530

    Added all Primary and Secondary GameplayTags through C++ RPG_GameplayTags.

--- Source/GAS_RPG/Private/RPG_GameplayTags.cpp
+++ Source/GAS_RPG/Private/RPG_GameplayTags.cpp
@@ -10,5 +10,21 @@ FRPG_GameplayTags FRPG_GameplayTags::GameplayTags;
 
 void FRPG_GameplayTags::InitializeNativeGameplayTags()
 {
-	GameplayTags.Attributes_Secondary_Armor = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.Armor"));
+	//Primary Attributes
+	GameplayTags.Attributes_Primary_Strength = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Primary.Strength"), FString("Influences physical power and combat effectiveness."));
+	GameplayTags.Attributes_Primary_Intelligence = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Primary.Intelligence"), FString("Affects magical power and spellcasting ability."));
+	GameplayTags.Attributes_Primary_Resilience = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Primary.Resilience"), FString("Provides resistance to various forms of damage."));
+	GameplayTags.Attributes_Primary_Vigor = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Primary.Vigor"), FString("Enhances overall vitality and endurance."));
+
+	//Secondary Attributes
+	GameplayTags.Attributes_Secondary_Armor = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.Armor"), FString("Reduce the damage taken"));
+	GameplayTags.Attributes_Secondary_ArmorPenetration = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.ArmorPenetration"), FString("Increase the penetration of enemy armor"));
+	GameplayTags.Attributes_Secondary_BlockChance = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.BlockChance"), FString("Chance to block incoming attacks"));
+	GameplayTags.Attributes_Secondary_CriticalHitChance = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.CriticalHitChance"), FString("Chance to land a critical hit"));
+	GameplayTags.Attributes_Secondary_CriticalHitDamage = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.CriticalHitDamage"), FString("Increase damage dealt on critical hits"));
+	GameplayTags.Attributes_Secondary_CriticalHitResistance = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.CriticalHitResistance"), FString("Reduce damage received from critical hits"));
+	GameplayTags.Attributes_Secondary_HealthRegeneration = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.HealthRegeneration"), FString("Increase health regeneration over time"));
+	GameplayTags.Attributes_Secondary_ManaRegeneration = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.ManaRegeneration"), FString("Increase mana regeneration over time"));
+	GameplayTags.Attributes_Vital_MaxHealth = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.MaxHealth"), FString("Maximum health a character can have"));
+	GameplayTags.Attributes_Vital_MaxMana = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.MaxMana"), FString("Maximum mana a character can have"));
 }
--- Source/GAS_RPG/Public/RPG_GameplayTags.h
+++ Source/GAS_RPG/Public/RPG_GameplayTags.h
@@ -17,7 +17,23 @@ public:
 	static const FRPG_GameplayTags& Get() { return GameplayTags; }
 	static void InitializeNativeGameplayTags();
 
-	FGameplayTag Attributes_Secondary_Armor;
+	//Primary Attributes
+	FGameplayTag Attributes_Primary_Strength; // Represents the primary attribute for Strength, influencing physical power.
+	FGameplayTag Attributes_Primary_Intelligence; // Represents the primary attribute for Intelligence, influencing magical power.
+	FGameplayTag Attributes_Primary_Resilience; // Represents the primary attribute for Resilience, providing resistance to various forms of damage.
+	FGameplayTag Attributes_Primary_Vigor; // Represents the primary attribute for Vigor, enhancing overall vitality and endurance.
+
+	//Secondary Attributes
+	FGameplayTag Attributes_Secondary_Armor; // Represents the secondary attribute for Armor, reducing the damage taken.
+	FGameplayTag Attributes_Secondary_ArmorPenetration; // Represents the secondary attribute for Armor Penetration, increasing damage dealt to armored targets.
+	FGameplayTag Attributes_Secondary_BlockChance; // Represents the secondary attribute for Block Chance, the chance to block incoming attacks.
+	FGameplayTag Attributes_Secondary_CriticalHitChance; // Represents the secondary attribute for Critical Hit Chance, the chance to land a critical hit.
+	FGameplayTag Attributes_Secondary_CriticalHitDamage; // Represents the secondary attribute for Critical Hit Damage, increasing damage on critical hits.
+	FGameplayTag Attributes_Secondary_CriticalHitResistance; // Represents the secondary attribute for Critical Hit Resistance, reducing damage taken from critical hits.
+	FGameplayTag Attributes_Secondary_HealthRegeneration; // Represents the secondary attribute for Health Regeneration, the rate at which health is restored over time.
+	FGameplayTag Attributes_Secondary_ManaRegeneration; // Represents the secondary attribute for Mana Regeneration, the rate at which mana is restored over time.
+	FGameplayTag Attributes_Vital_MaxHealth; // Represents the vital attribute for Maximum Health, the total health pool.
+	FGameplayTag Attributes_Vital_MaxMana; // Represents the vital attribute for Maximum Mana, the total mana pool.
 	
 protected:
 

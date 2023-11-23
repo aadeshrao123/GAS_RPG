90fea96a0ef0ed6b0a9ec24c68e960948f956e53
Aadesh Yadav <55663044+aadeshrao123@users.noreply.github.com>
2023-11-23 10:56:37 +0530

    Created a new GameplayTags class which will contain the Gameplay Tags.

---
+++ Source/GAS_RPG/Private/RPG_GameplayTags.cpp
@@ -0,0 +1,14 @@
+﻿// Copyright AadeshRao
+
+
+#include "RPG_GameplayTags.h"
+#include "GameplayTagsManager.h"
+
+
+
+FRPG_GameplayTags FRPG_GameplayTags::GameplayTags;
+
+void FRPG_GameplayTags::InitializeNativeGameplayTags()
+{
+	UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.Armor"));
+}
---
+++ Source/GAS_RPG/Public/RPG_GameplayTags.h
@@ -0,0 +1,25 @@
+﻿// Copyright AadeshRao
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+
+/*
+ *RPG_GameplayTags
+ *Singleton containing native Gameplay Tags 
+ * 
+ */
+
+struct FRPG_GameplayTags
+{
+public:
+	static const FRPG_GameplayTags& Get() { return GameplayTags; }
+	static void InitializeNativeGameplayTags();
+	
+protected:
+
+private:
+	static FRPG_GameplayTags GameplayTags;
+	
+};

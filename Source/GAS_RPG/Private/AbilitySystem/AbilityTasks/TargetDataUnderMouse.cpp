fe508964a7c097a6d7f59f504f6963994fe38896
Aadesh Yadav <55663044+aadeshrao123@users.noreply.github.com>
2023-12-12 23:16:20 +0530

    Created our own AbilityTask which will give use a a Fvector from under our mouse click.

---
+++ Source/GAS_RPG/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
@@ -0,0 +1,26 @@
+﻿// Copyright AadeshRao
+
+
+#include "AbilitySystem/AbilityTasks/TargetDataUnderMouse.h"
+
+#include "AbilitySystemComponent.h"
+
+UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility)
+{
+	UTargetDataUnderMouse* MyObj = NewAbilityTask<UTargetDataUnderMouse>(OwningAbility);
+	
+	return MyObj;
+}
+
+void UTargetDataUnderMouse::Activate()
+{
+	Super::Activate();
+	APlayerController* Controller = Ability->GetCurrentActorInfo()->PlayerController.Get();
+	FHitResult HitResult;
+	Controller->GetHitResultUnderCursor(ECC_Visibility, false, HitResult);
+	if (HitResult.bBlockingHit)
+	{
+		ValidData.Broadcast(HitResult.ImpactPoint);
+	}
+	
+}

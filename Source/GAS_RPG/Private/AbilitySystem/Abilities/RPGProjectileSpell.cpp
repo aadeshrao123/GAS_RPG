53e1166e4d959a9d787c3e027aa6d8270f7204e0
Aadesh Yadav <55663044+aadeshrao123@users.noreply.github.com>
2023-12-12 12:25:56 +0530

    Added a new Gameplay Ability RPGProjectileSpell and created a blueprint class derrived from it GA_FireBolt and tested it with a print string.

---
+++ Source/GAS_RPG/Private/AbilitySystem/Abilities/RPGProjectileSpell.cpp
@@ -0,0 +1,15 @@
+﻿// Copyright AadeshRao
+
+
+#include "AbilitySystem/Abilities/RPGProjectileSpell.h"
+
+#include "Kismet/KismetSystemLibrary.h"
+
+void URPGProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+                                          const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+                                          const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	UKismetSystemLibrary::PrintString(this, FString("ActivateAbility C++"), true, true, FLinearColor::Red, 3.f);
+}
---
+++ Source/GAS_RPG/Public/AbilitySystem/Abilities/RPGProjectileSpell.h
@@ -0,0 +1,19 @@
+﻿// Copyright AadeshRao
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RPGGameplayAbility.h"
+#include "RPGProjectileSpell.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_RPG_API URPGProjectileSpell : public URPGGameplayAbility
+{
+	GENERATED_BODY()
+protected:
+	
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+};

4be5bb22eb5ad9431d52419cab4f3d0500115db3
Aadesh Yadav <55663044+aadeshrao123@users.noreply.github.com>
2023-12-24 12:02:31 +0530

    Created a new C++ class UExecCalc_Damage for calculating our damage. It's a more advanced class than ModMacCalc. In this we can change several different Gameplay Attributes at once.

---
+++ Source/GAS_RPG/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -0,0 +1,22 @@
+﻿// Copyright AadeshRao
+
+
+#include "AbilitySystem/ExecCalc/ExecCalc_Damage.h"
+
+#include "AbilitySystemComponent.h"
+
+UExecCalc_Damage::UExecCalc_Damage()
+{
+}
+
+void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
+	FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{
+	const UAbilitySystemComponent* SourceASC= ExecutionParams.GetSourceAbilitySystemComponent();
+	const UAbilitySystemComponent* TargetASC= ExecutionParams.GetTargetAbilitySystemComponent();
+
+	const AActor* SourceActor = SourceASC ? SourceASC->GetAvatarActor() : nullptr;
+	const AActor* TargetActor = TargetASC ? TargetASC->GetAvatarActor() : nullptr;
+
+	FGameplayEffectSpec EffectSpec = ExecutionParams.GetOwningSpec();
+}

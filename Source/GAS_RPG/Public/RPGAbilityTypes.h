796aba84400072f21d1eb46cd163b13cdc224bc2
Aadesh Yadav <55663044+aadeshrao123@users.noreply.github.com>
2023-12-27 10:24:37 +0530

    Created a new Class for our custom FGameplayContextHandle in RPGAnilityTypes added two new boll Variables in the struct and NetSerialixe it for Multiplayer.

---
+++ Source/GAS_RPG/Public/RPGAbilityTypes.h
@@ -0,0 +1,35 @@
+﻿// Copyright AadeshRao
+
+#pragma once
+#include "GameplayEffectTypes.h"
+#include "RPGAbilityTypes.generated.h"
+
+USTRUCT(BlueprintType)
+struct FRPGGameplayEffectContext : public FGameplayEffectContext
+{
+	GENERATED_BODY()
+public:
+
+	bool IsBlockedHit() const {return bIsBlockedHit; }
+	bool IsCriticalHit() const {return bIsCriticalHit; }
+
+	void SetIsBlockedHit(bool bIsInBlockedHit) {bIsBlockedHit = bIsInBlockedHit; }
+	void SetIsCriticalHit(bool bIsInCriticalHit) {bIsCriticalHit = bIsInCriticalHit; }
+	
+	
+	virtual UScriptStruct* GetScriptStruct() const
+	{
+		return FGameplayEffectContextHandle::StaticStruct();
+	}
+
+	virtual bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess);
+	
+protected:
+	UPROPERTY()
+	bool bIsBlockedHit;
+
+	UPROPERTY()
+	bool bIsCriticalHit;
+
+	
+};

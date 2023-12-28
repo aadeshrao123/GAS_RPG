81565e0b3e8ab3c353c8b48afb2a2e83a63f47fd
Aadesh Yadav <55663044+aadeshrao123@users.noreply.github.com>
2023-12-28 13:32:50 +0530

    Added functionality for making our Enemy find Player's nearest Location and then follow it.

--- Source/GAS_RPG/Private/AI/BTService_FindNearestPlayer.cpp
+++ Source/GAS_RPG/Private/AI/BTService_FindNearestPlayer.cpp
@@ -4,11 +4,37 @@
 #include "AI/BTService_FindNearestPlayer.h"
 
 #include "AIController.h"
+#include "BehaviorTree/BTFunctionLibrary.h"
+#include "Kismet/GameplayStatics.h"
 
 void UBTService_FindNearestPlayer::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
 {
 	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
 
-	GEngine->AddOnScreenDebugMessage(1, 1.f, FColor::Green, *AIOwner->GetName());
-	GEngine->AddOnScreenDebugMessage(2, 1.f, FColor::Green, *ActorOwner->GetName());
+	APawn* Pawn = AIOwner->GetPawn();
+
+	const FName TargetTag = Pawn->ActorHasTag(FName("Player")) ? FName("Enemy") : FName("Player");
+
+	TArray<AActor*> ActorsWithTag;
+	UGameplayStatics::GetAllActorsWithTag(Pawn, TargetTag, ActorsWithTag);
+
+	float ClosestDistance = TNumericLimits<float>::Max();
+	AActor* ClosestActor = nullptr;
+	
+	for (AActor* Actor : ActorsWithTag)
+	{
+		GEngine->AddOnScreenDebugMessage(2, 0.5f, FColor::Green, *Actor->GetName());
+
+		if (IsValid(Actor) && IsValid(Pawn))
+		{
+			const float Distance = Pawn->GetDistanceTo(Actor);
+			if (Distance < ClosestDistance)
+			{
+				ClosestDistance = Distance;
+				ClosestActor = Actor;
+			}
+		}
+	}
+	UBTFunctionLibrary::SetBlackboardValueAsObject(this, TargetToFollowSelector, ClosestActor);
+	UBTFunctionLibrary::SetBlackboardValueAsFloat(this, DistanceToTargetSelector, ClosestDistance);
 }
--- Source/GAS_RPG/Public/AI/BTService_FindNearestPlayer.h
+++ Source/GAS_RPG/Public/AI/BTService_FindNearestPlayer.h
@@ -15,4 +15,10 @@ class GAS_RPG_API UBTService_FindNearestPlayer : public UBTService_BlueprintBase
 	GENERATED_BODY()
 protected:
 	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	FBlackboardKeySelector TargetToFollowSelector;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	FBlackboardKeySelector DistanceToTargetSelector;
 };

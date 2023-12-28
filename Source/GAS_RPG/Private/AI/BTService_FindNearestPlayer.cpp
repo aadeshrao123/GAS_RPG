d183803fc0eed05d2bc895e8a36603ce1f3d9185
Aadesh Yadav <55663044+aadeshrao123@users.noreply.github.com>
2023-12-28 12:50:01 +0530

    Added a new Service class for Behaviour tree for finding the nearest player location and play tested it by printing a string.

---
+++ Source/GAS_RPG/Private/AI/BTService_FindNearestPlayer.cpp
@@ -0,0 +1,14 @@
+﻿// Copyright AadeshRao
+
+
+#include "AI/BTService_FindNearestPlayer.h"
+
+#include "AIController.h"
+
+void UBTService_FindNearestPlayer::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	GEngine->AddOnScreenDebugMessage(1, 1.f, FColor::Green, *AIOwner->GetName());
+	GEngine->AddOnScreenDebugMessage(2, 1.f, FColor::Green, *ActorOwner->GetName());
+}
---
+++ Source/GAS_RPG/Public/AI/BTService_FindNearestPlayer.h
@@ -0,0 +1,18 @@
+﻿// Copyright AadeshRao
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Services/BTService_BlueprintBase.h"
+#include "BTService_FindNearestPlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_RPG_API UBTService_FindNearestPlayer : public UBTService_BlueprintBase
+{
+	GENERATED_BODY()
+protected:
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+};

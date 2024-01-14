b8fcaf06db2c74a9aaf28d2803ef140278230238
Aadesh Yadav <55663044+aadeshrao123@users.noreply.github.com>
2024-01-14 14:59:27 +0530

    Create a new Gameplay ABility and wrote a function which will give a random spawn location in a specified area.

---
+++ Source/GAS_RPG/Private/AbilitySystem/Abilities/RPGSummonAbility.cpp
@@ -0,0 +1,24 @@
+﻿// Copyright AadeshRao
+
+
+#include "AbilitySystem/Abilities/RPGSummonAbility.h"
+
+#include "Kismet/KismetSystemLibrary.h"
+
+TArray<FVector> URPGSummonAbility::GetSpawnLocations()
+{
+	const FVector ForwardVector = GetAvatarActorFromActorInfo()->GetActorForwardVector();
+	const FVector Location = GetAvatarActorFromActorInfo()->GetActorLocation();
+	const float DeltaSpread = SpawnSpread / NumMinions;
+
+	const FVector LeftOfSpread = ForwardVector.RotateAngleAxis(-SpawnSpread / 2.f, FVector::UpVector);
+
+	TArray<FVector> SpawnLocations;
+	for (int32 i = 0; i < NumMinions; i++)
+	{
+		const FVector Direction = LeftOfSpread.RotateAngleAxis((DeltaSpread * i), FVector::UpVector);
+		const FVector ChosenSpawnLocation = Location + Direction * FMath::FRandRange(MinSpawnDistance, MaxSpawnDistance);
+		SpawnLocations.Add(ChosenSpawnLocation);
+	}
+	return SpawnLocations;
+}
---
+++ Source/GAS_RPG/Public/AbilitySystem/Abilities/RPGSummonAbility.h
@@ -0,0 +1,36 @@
+﻿// Copyright AadeshRao
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RPGGameplayAbility.h"
+#include "RPGSummonAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_RPG_API URPGSummonAbility : public URPGGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Summoning")
+	TArray<FVector> GetSpawnLocations();
+
+	UPROPERTY(EditAnywhere, Category = "Summoning")
+	int32 NumMinions = 5;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Summoning")
+	TArray<TSubclassOf<APawn>> MinionClasses;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Summoning")
+	float MinSpawnDistance = 50.f;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Summoning")
+	float MaxSpawnDistance = 250.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Summoning")
+	float SpawnSpread = 90.f;
+	
+};

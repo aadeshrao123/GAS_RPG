77d4dcc7af64a1785dfc10713356ae7b176ec652
Aadesh Yadav <55663044+aadeshrao123@users.noreply.github.com>
2024-01-24 12:31:10 +0530

    Added a Async WaitForCooldownChange class for handleing the cooldown time. So we will not be able to execute the same ability in that cooldown period.

---
+++ Source/GAS_RPG/Public/AbilitySystem/WaitCooldownChange.h
@@ -0,0 +1,45 @@
+﻿// Copyright AadeshRao
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "ActiveGameplayEffectHandle.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "WaitCooldownChange.generated.h"
+
+class UAbilitySystemComponent;
+struct FGameplayEffectSpec;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FCooldownChangeSignature, float, TimeRemaning);
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_RPG_API UWaitCooldownChange : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(BlueprintAssignable)
+	FCooldownChangeSignature CoolDownStart;
+
+	UPROPERTY(BlueprintAssignable)
+	FCooldownChangeSignature CooldownEnd;
+
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = true))
+	static UWaitCooldownChange* WaitForCooldownChange(UAbilitySystemComponent* AbilitySystemComponent, const FGameplayTag& InCooldownTag);
+
+	UFUNCTION(BlueprintCallable)
+	void EndTask();
+
+protected:
+	UPROPERTY()
+	TObjectPtr<UAbilitySystemComponent> ASC;
+
+	FGameplayTag CooldownTag;
+
+	void CooldownTagChanged(const FGameplayTag InCoolDownTag, int32 NewCount);
+
+	void OnActiveEffectAdded(UAbilitySystemComponent* TargetASC, const FGameplayEffectSpec& SpecApplied, FActiveGameplayEffectHandle ActiveEffectHandle);
+};

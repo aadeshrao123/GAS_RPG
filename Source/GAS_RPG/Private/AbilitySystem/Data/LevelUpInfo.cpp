1b457784923e2df2b1528429e3fe769a9fc57ce4
Aadesh Yadav <55663044+aadeshrao123@users.noreply.github.com>
2024-02-02 11:41:38 +0530

    Added a new Data Asset for keeping the track of our Levels and how much rewards we will get when Leveled up.

---
+++ Source/GAS_RPG/Private/AbilitySystem/Data/LevelUpInfo.cpp
@@ -0,0 +1,23 @@
+﻿// Copyright AadeshRao
+
+
+#include "AbilitySystem/Data/LevelUpInfo.h"
+
+int32 ULevelUpInfo::FindLevelForXP(int32 XP)
+{
+	int32 Level = 1;
+	bool bSearching = true;
+	while (bSearching)
+	{
+		if (LevelUpInfo.Num() - 1 <= Level) return Level;
+		if (XP >= LevelUpInfo[Level].LevelUPRequirement)
+		{
+			++Level;
+		}
+		else
+		{
+			bSearching = false;
+		}
+	}
+	return Level;
+}
---
+++ Source/GAS_RPG/Public/AbilitySystem/Data/LevelUpInfo.h
@@ -0,0 +1,38 @@
+﻿// Copyright AadeshRao
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "LevelUpInfo.generated.h"
+
+USTRUCT(BlueprintType)
+struct FRPGLevelUpInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly)
+	int32 LevelUPRequirement = 0;
+
+	UPROPERTY(EditDefaultsOnly)
+	int32 AttributePointRewards = 1;
+
+	UPROPERTY(EditDefaultsOnly)
+	int32 SpellPointReward = 1;
+};
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_RPG_API ULevelUpInfo : public UDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	
+	UPROPERTY(EditDefaultsOnly, Category = "LevelUP")
+	TArray<FRPGLevelUpInfo> LevelUpInfo;
+
+	int32 FindLevelForXP(int32 XP);
+};
